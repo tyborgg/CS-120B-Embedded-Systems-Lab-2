2179065016c88e8a6b87be65a50a53cbef6eecef
Tyler Pastor <tpast001@wch136-24.cs.ucr.edu>
2021-04-07 16:01:54 -0700

    Finished practice reading from input pins

--- source/main.c
+++ source/main.c
@@ -16,7 +16,15 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 
     /* Insert your solution below */
-    
+	DDRA = 0x00; PORTA = 0xFF; 	// Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; 	// Configure port B's 8 pins as outputs
+								// Initialize output on PORTB to 0x00	
+	unsigned char temp_input = 0x00;
+	while(1) {
+		temp_input = PINA;
+		PORTB = temp_input;	// Writes port B's 8 pins with the values
+ 							// on port A's 8 pins
+		}				
 
     return 1;
 }
