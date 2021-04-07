e922bac4361b06e927972a4864382289d92e60f8
Tyler Pastor <tpast001@wch136-24.cs.ucr.edu>
2021-04-07 15:33:10 -0700

    Initializing repository with lab files

---
+++ source/main.c
@@ -0,0 +1,22 @@
+/*	Author: tpast001
+ *  Partner(s) Name: N/A
+ *	Lab Section: 023
+ *	Assignment: Lab #2  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    
+
+    return 1;
+}
