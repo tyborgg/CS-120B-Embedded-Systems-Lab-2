e6720845c6ac63ba92e7960914ec6c05058188e0
Tyler Pastor <tpast001@wch136-24.cs.ucr.edu>
2021-04-07 16:27:13 -0700

    Finished bit masking and writing running practice test cases

--- source/main.c
+++ source/main.c
@@ -16,15 +16,25 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 
     /* Insert your solution below */
-	DDRA = 0x00; PORTA = 0xFF; 	// Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; 	// Configure port B's 8 pins as outputs
-								// Initialize output on PORTB to 0x00	
-	unsigned char temp_input = 0x00;
+    	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
 	while(1) {
-		temp_input = PINA;
-		PORTB = temp_input;	// Writes port B's 8 pins with the values
- 							// on port A's 8 pins
-		}				
+		// 1) Read input
+		tmpA = PINA & 0x01;
+		// 2) Perform computation
+		// if PA0 is 1, set PB1PB0 = 01, else = 10
+		if (tmpA == 0x01) { // True if PA0 is 1
+			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+							 // (clear rightmost 2 bits, then set to 01)
+		} else {
+			tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
+							 // (clear rightmost 2 bits, then set to 10)
+		}	
+		// 3) Write output
+		PORTB = tmpB;	
+	}
 
-    return 1;
+    return 0;
 }
