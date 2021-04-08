3ce5c953bae96eb56913a8a83c0d90a0e8cf096f
Tyler Pastor <tpast001@wch136-24.cs.ucr.edu>
2021-04-07 23:58:05 -0700

    Completed part 4

--- source/main.c
+++ source/main.c
@@ -19,21 +19,33 @@ int main(void) {
 	DDRD = 0xFF; PORTD = 0x00;	
 	unsigned char tempAdd = 0;
 	unsigned char tempSub = 0;
+	unsigned char tmpA = 0;
+	unsigned char tmpC = 0;
 
 	while(1){
 		tempAdd = 0;
 		tempSub = 0;		
 		tempAdd = PINA + PINB + PINC;
-		tempSub = PINA - PINC;
+		tmpA = PINA;
+		tmpC = PINC;
+		if(tmpA > tmpC){
+			tempSub = tmpA - tmpC;
+		}
+		else{
+			tempSub = tmpC - tmpA;
+		}
 
 		if(tempAdd > 140){
 			PORTD = 0x01;
+		}
+		else{
+			PORTD = 0x00;
 		}
 		if(tempSub > 80){
 			PORTD = PORTD | 0x02;
 		}
 
-		PORTD = PORTD | (tempAdd & 0xFC);
+		PORTD = PORTD | ((tempAdd >> 2) & 0xFC);
 	}
 
     return 0;
