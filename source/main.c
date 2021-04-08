29c226283fb60735fa2bb0ffa43704d41564c236
Tyler Pastor <tpast001@wch136-24.cs.ucr.edu>
2021-04-07 18:44:21 -0700

    Completed part 3

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: tpast001
  *  Partner(s) Name: N/A
  *	Lab Section: 023
- *	Assignment: Lab #2  Exercise #
+ *	Assignment: Lab #2  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,12 +14,13 @@
 
 int main(void) {	
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTB = 0x00;	
+	DDRC = 0xFF; PORTC = 0x00;	
 	unsigned char tmpA0;
 	unsigned char tmpA1;
 	unsigned char tmpA2;
 	unsigned char tmpA3;
 	unsigned char cntavail;
+	unsigned char cnt;
 
 	while(1){
 		tmpA0 = PINA & 0x01;
@@ -27,21 +28,33 @@ int main(void) {
 		tmpA2 = PINA & 0x04;
 		tmpA3 = PINA & 0x08;
 		cntavail = 0;
+		cnt = 15;
 
 		if(tmpA0 == 0){
 			cntavail++;
+			cnt -= 1;
 		}
 		if(tmpA1 == 0){
 			cntavail++;
+			cnt -= 2;
 		}
 		if(tmpA2 == 0){
 			cntavail++;
+			cnt -= 4;
 		}
 		if(tmpA3 == 0){
 			cntavail++;
+			cnt -= 8;
 		}
 		
-		PORTC = cntavail;
+		if(cntavail == 0){
+			PORTC = 0x8F;	
+		}
+		else{
+			PORTC = cnt;
+			PORTC = PORTC & 0x0F;
+		}
+			
 	}
 
     return 0;
